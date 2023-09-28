6dc6ed19eb18c4e9a874d426595fffe6e5d6e15d
IreneDeNevi <denevi.irene@gmail.com>
2023-09-28 12:01:29 +0200

    Update MonteCarloes1.py

--- MonteCarloes1.py
+++ MonteCarloes1.py
@@ -1,33 +1,54 @@
+# Importa il modulo ROOT e il modulo math per utilizzare le funzioni e le classi necessarie.
 from ROOT import *
 import math as m
 
+# Inizializza un generatore di numeri casuali TRandom3 e imposta il seme.
 rnd = TRandom3()
 rnd.SetSeed(12345678)
 
-ntot = 1000 #numero totale di eventi da generare
-tau1 = 0.1
-f= 0.3
-tau2 = 1
-tacc = 0.01
+# Definizione del numero totale di eventi da generare.
+ntot = 1000
 
+# Definizione dei parametri per la generazione degli eventi.
+tau1 = 0.1  # Tempo di vita 1
+f = 0.3  # Frazione degli eventi con tau1
+tau2 = 1  # Tempo di vita 2
+tacc = 0.01  # Tempo di accettazione
 
-h = TH1D("h","",100,0,3)
-hm = TH1D("hm","",100,0,3)
+# Creazione di due istanze di istogrammi TH1D.
+h = TH1D("h", "", 100, 0, 3)  # Istogramma principale
+hm = TH1D("hm", "", 100, 0, 3)  # Istogramma per gli eventi accettati
 
-for i in range(0,ntot):
-        tau = 0.0
-        if rnd.Rndm()<f:
-                tau = tau1
-        else:
-                tau = tau2
-        t = -tau*m.log(1-rnd.Rndm())
-        if rnd.Rndm()<(1-m.exp(-t/tacc)):
-                hm.Fill(t)
-        h.Fill(t)
+# Ciclo attraverso la generazione degli eventi.
+for i in range(0, ntot):
+    tau = 0.0
+
+    # Genera casualmente un valore di tau1 o tau2 in base alla frazione f.
+    if rnd.Rndm() < f:
+        tau = tau1
+    else:
+        tau = tau2
+
+    # Genera un tempo di decadimento t da una distribuzione esponenziale.
+    t = -tau * m.log(1 - rnd.Rndm())
+
+    # Verifica se l'evento viene accettato in base a una certa probabilità.
+    if rnd.Rndm() < (1 - m.exp(-t / tacc)):
+        hm.Fill(t)  # Aggiunge il tempo t all'istogramma degli eventi accettati.
+    h.Fill(t)  # Aggiunge il tempo t all'istogramma principale.
+
+# Disegna l'istogramma principale.
 h.Draw()
+
+# Disegna l'istogramma degli eventi accettati sopra quello principale.
 hm.Draw("SAME")
+
+# Imposta il colore dell'istogramma degli eventi accettati a blu.
 hm.SetLineColor(2)
+
+# Aggiorna la visualizzazione del canvas.
 gPad.Modified()
 gPad.Update()
-gApplication.Run(True)
 
+# Esegue l'applicazione per visualizzare il risultato.
+gApplication.Run(True)
